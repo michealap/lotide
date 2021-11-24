40105931185673c8cdbdbcf21e87fcf3f00a8a16
Micheala Palmer <88060629+michealap@users.noreply.github.com>
2021-11-23 22:40:17 -0500

    update without function
    refactored nested loop to use .includes()

--- without.js
+++ without.js
@@ -12,32 +12,17 @@ const eqArrays = function(firstList, secondList) {
 };
 
 const without = function(source, itemsToRemove) {
-  //declare an empty array for after items are removed
   const sourceWithout = [];
-  //nested loop to check the first element of 1st array against all elements of 2nd array
-  //then move to the consequent elements of 1st array
   for (let i = 0; i < source.length; i++) {
-    //console.log('source:',source[i]) --> check what's being passed;
-    //iterate through first array
-    let found = false;
-      
-    for (let j = 0; j < itemsToRemove.length; j++) {
-      //console.log('items to remove:', itemsToRemove[j]) --> check what's being passed;
-      //iterate thorugh 2nd array
-
-      if (source[i] === itemsToRemove[j]) {
-        //if item in 1st array matches 2nd array then set to true and exit loop
-        found = true;
-        break;
-      }
-    }
-    if (found === false) {
+    //loop through the length of 1st array
+    if (!itemsToRemove.includes(source[i])) {
       sourceWithout.push(source[i]);
     }
   }
   console.log(sourceWithout);
   return sourceWithout;
 };
+
 const assertArraysEqual = function(actual, expected) {
   const isEqual = eqArrays(actual,expected);
   if (isEqual) {
