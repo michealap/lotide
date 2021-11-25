ae0e1f8c4d68e614b44307068369ebba2902a4d9
Micheala Palmer <88060629+michealap@users.noreply.github.com>
2021-11-25 00:27:37 -0500

    Update head.js

--- head.js
+++ head.js
@@ -1,17 +1,19 @@
-/* */
+/* retrieve the first element from the array returning the element itself and testing using assertEqual function*/
 const head = function(array) {
   return (array != null && array.length)
   ? array[0]
   : undefined
 };
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`${String.fromCodePoint(128512)} Assertion Passed: ${actual} === ${expected}`);
   } else console.log(`${String.fromCodePoint(128148)} Assertion Failed: ${actual} !== ${expected}`);
 };
 
+//Test Cases
 assertEqual(head([5,6,7]), 5);
 assertEqual(head([5,6,7]), 7);
 assertEqual(head([]), 5); //returns undefined
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["Labs"]), "Hello");//returns the only element
+assertEqual(head(["Labs"]), "Hello");//returns the only element 
