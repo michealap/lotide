739fd4d79c905b6ccd865c5fa5ab5704b9b60e69
Micheala Palmer <88060629+michealap@users.noreply.github.com>
2021-11-24 22:26:19 -0500

    code lint and update eqArray()
    aim: check for same numbers of keys and perfect match

--- eqObjects.js
+++ eqObjects.js
@@ -11,56 +11,16 @@ Within our loop, we can use Array.isArray against both values to see if array co
 If both values are indeed arrays: pass them to eqArrays and ensure that it returns true.
 Otherwise (else): assume that they are primitives and continue to use === to compare the two values.
 */
-
-
-
-// Returns true if both objects have identical keys with identical values.
-// Otherwise you get back a big fat false!
-function eqArrays(arr1, arr2) {
-  let var1 = toString(arr1);
-  let var2 = toString(arr2);
-  
-  if (var1 === var2) {
-    return true;
-  } else {
+const eqArrays = function(firstList, secondList) {
+  if (firstList.length !== secondList.length) {
     return false;
   }
-};
-const eqObjects = function(object1, object2) {
-  //variable
-  ifTrue = false
-  // check if the length of the two objects keys are the same, if they are not the same it defaults to false
-  if (Object.keys(object1).length !== Object.keys(object2).length) {
-    return false
-  }
-  // cycle through keys of object1 loop
-  for (let key of Object.keys(object1)) {
-    // if both [keys] are arrays, progress
-    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      //both [keys] are arrays, use eqArrays to check if they are equal, and if equal returns true, checks if true === true and returns true to our variable
-      if (eqArrays(object1[key],object2[key]) === true) {
-        ifTrue = true
-      }
-      // both [keys] are arrays, however eqArray did not pass, therefore it will be false
-      else {
-        return false
-      }
-// checks to see if obeject1 at [key] is an array, or if object2 at [key] is an array, using Or, we can find that they will not be matching data types therefore will be false
-    }
-    else if (Array.isArray(object1[key]) || Array.isArray(object2)[key]) {
-      return false
-    }
-
-// Returns true if both objects have identical key with identical values.
-    else if (object1[key] === object2[key]) {
-      ifTrue = true
-    }
-    // Otherwise you get false!
-    else {
-      return false
+  for (let i = 0; i < firstList.length; i++) {
+    if (firstList[i] !== secondList[i]) {
+      return false;
     }
   }
-  return ifTrue
+  return true;
 };
 
 const assertEqual = function(actual, expected) {
@@ -68,6 +28,40 @@ const assertEqual = function(actual, expected) {
     console.log(`${String.fromCodePoint(128512)} Assertion Passed: ${actual} === ${expected}`);
   } else console.log(`${String.fromCodePoint(128148)} Assertion Failed: ${actual} !== ${expected}`);
 };
+
+const eqObjects = function(object1, object2) {
+  //variable
+  let ifTrue = false;
+  // check if the length of the two objects keys are the same, if they are not the same it defaults to false
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+  // cycle through keys of object1 loop
+  for (let key of Object.keys(object1)) {
+    // if both [keys] are arrays, progress
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      //both [keys] are arrays, use eqArrays to check if they are equal, and if equal returns true, checks if true === true and returns true to our variable
+      if (eqArrays(object1[key],object2[key]) === true) {
+        ifTrue = true;
+      } else {
+        // both [keys] are arrays, however eqArray did not pass, therefore it will be false
+        return false;
+      }
+      // checks to see if object1 at [key] is an array, or if object2 at [key] is an array, using Or, we can find that they will not be
+      //matching data types therefore will be false
+    } else if (Array.isArray(object1[key]) || Array.isArray(object2)[key]) {
+      return false;
+    } else if (object1[key] === object2[key]) {
+      // Returns true if both objects have identical key with identical values.
+      ifTrue = true;
+    } else {
+      return false;
+    }
+  }
+  return ifTrue;
+};
+
+//Test Cases
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 assertEqual(eqObjects(ab, ba), true); // => true
